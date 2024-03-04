33f1052216f949371ef60eadc8a357e8f54a5e7e
Camélien <camelien.tournu@etudiant.univ-reims.fr>
2024-03-04 12:23:49 +0100

    fix/ delete register view unused

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -46,11 +46,6 @@ const router = createRouter({
       component: () => import('@/views/LoginView.vue')
     },
     {
-      path: '/register',
-      name: 'register',
-      component: () => import('@/views/RegisterView.vue')
-    },
-    {
       path: '/accueil',
       name: 'accueil',
       component: () => import('@/views/AccueilView.vue')
