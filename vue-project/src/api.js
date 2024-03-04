7acfa13a237aeeb5d8045d37acbabb082b7ba4a8
Camélien <camelien.tournu@etudiant.univ-reims.fr>
2024-03-04 20:37:13 +0100

    fix/ add s to http

--- vue-project/src/api.js
+++ vue-project/src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://movieapi.camelientrn.fr/api',
+  baseURL: 'https://movieapi.camelientrn.fr/api',
   timeout: 10000,
 });
 
